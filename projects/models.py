4870a044c6240d79dcd1c5de67ec0f879aac4649
eiire <bulax.d.2101@gmail.com>
2020-02-27 22:11:02 +0700

    "Create_my_portfolio_with_projects_and_blog"

---
+++ projects/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    technology = models.CharField(max_length=30)
+    image = models.FilePathField(path="static/img")
+
